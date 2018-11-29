2f0894ee27ba75cca1a97b107b3c619b47bbf00a
Gertjan <gertjan_rocks@hotmail.com>
2018-11-29 21:36:31 +0100

    Added test

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+var exec = require("child_process").exec;
+var Accessory, Service, Characteristic, UUIDGen;
+
+module.exports = function (homebridge) {
+  Accessory = homebridge.platformAccessory;
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  UUIDGen = homebridge.hap.uuid;
+
+  homebridge.registerPlatform("homebridge-commander", "commander", commanderPlatform, true);
+}
+
+function commanderPlatform(log, config, api) {
+  log("Commander Platform init");
+  // Register the log
+  this.log = log;
+
+  // Get all the commands
+  this.config = config || {"platform": "commander"};
+  //this.commands = this.config.commands || [];
+
+  //Empty lists
+  this.commands = {};
+  this.polling = {};
+
+}
+
+commanderPlatform.prototype = {
+  accessories: function(callback) {
+    this.log("Reading config file");
+    
+    // Loop trough all commands to add
+    for (var i = 0; i < this.config.commands.length; i++)
+    {
+       this.log("Register %s",this.config.commands[i].name);
+    }
+
+    callback(this.commands);
   }
+}
